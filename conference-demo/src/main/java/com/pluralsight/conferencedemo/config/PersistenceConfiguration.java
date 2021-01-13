3c098bf16d88914ffaf2f89f66cf43fe7da59a2f
Zamsheer <77385806+Zamsheer@users.noreply.github.com>
2021-01-13 17:46:34 +0530

    Update PersistenceConfiguration.java
    Removed hard coded DB_URL values

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,12 +8,6 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder dataSourceBuilder= DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/conference_app");
-        dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("PostgreSQL");
-        return dataSourceBuilder.build();
+    
     }
 }
